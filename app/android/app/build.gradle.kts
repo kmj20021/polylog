ca869d1574eba9fbfcc4a9be5a67e11f44c285b2
MinjeKim <kmj20021@nate.com>
2026-06-04 19:51:48 +0900

    fn-recommend 카테고리 수정

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -3,10 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // ※ Google 로그인(메인 #3) 붙일 때 아래 + google-services.json 추가:
+    //    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.polylog"
+    namespace = "com.shingu.polylog"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,10 +22,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.polylog"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.shingu.polylog"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
--- app/android/app/src/main/kotlin/com/example/polylog/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.polylog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
